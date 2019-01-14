e78e0bc653951f7318d3ae833a35f95853795e85
Florian Lapolla <awflo1306@gmx.de>
2019-01-14 19:02:50 +0100

    First Lesson

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,3 +1,15 @@
 public class Hello {
 
+    public static void main(String[] args){
+        System.out.println("Hello, Flo!");
+
+        int myFirstNumber = 10;
+        int mySecondNumber = 20;
+        int myThirdNumber = 30;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+    }
+
 }

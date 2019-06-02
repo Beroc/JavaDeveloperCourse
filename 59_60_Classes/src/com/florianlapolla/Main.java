c874b089a51e25cf6ec5f5a7823bf2d7f0e9c2d9
Florian Lapolla <awflo1306@gmx.de>
2019-06-02 16:23:40 +0200

    Adding additional data for chapter 6

---
+++ 59_60_Classes/src/com/florianlapolla/Main.java
@@ -0,0 +1,13 @@
+package com.florianlapolla;
+
+public class Main {
+
+    public static void main(String[] args){
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.setModel("Porsche");
+        System.out.println("Model is: " + porsche.getModel());
+
+    }
+
+}
